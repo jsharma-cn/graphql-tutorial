a2fb86bbdd5576d0435cc0ba0f6f2651c1b62763
Sharma <jitendra_sharma@condenast.com>
2022-12-11 14:39:43 +0530

    Initial commit

---
+++ src/main/java/edu/jsharma/graphqltutorial/GraphqlTutorialApplication.java
@@ -0,0 +1,14 @@
+package edu.jsharma.graphqltutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlTutorialApplication.class, args);
+		System.out.println("Welcome to Java Wrold!!!");
+	}
+
+}
---
+++ src/test/java/edu/jsharma/graphqltutorial/GraphqlTutorialApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package edu.jsharma.graphqltutorial;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class GraphqlTutorialApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
